4d453f48ece1bb5707823c3c0577ed5b690f9868
Nathan-MSI <git.nathan@hotmail.com>
2020-01-24 17:52:46 +1100

    weight initialisation

--- DeepWizard/2hlayer.py
+++ DeepWizard/2hlayer.py
@@ -40,7 +40,7 @@ test_loader  = DataLoader(test_dataset, batch_size)
 
 # Step 3: Create Model Class
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_dim, layer_dim, output_size, bidir):
+    def __init__(self, input_size, hidden_dim, layer_dim, output_size, bidir, weight=0):
         super(LSTM, self).__init__()
 
         # Number of neurons in the hidden layer
@@ -54,11 +54,23 @@ class LSTM(nn.Module):
     
         # Generates the LSTM aspect to the model
         self.lstm = nn.LSTM(input_size, hidden_dim, layer_dim, batch_first = True, bidirectional=self.bidir)      # batch_first = True makes the output a tensor(batch, seq(*), feature)
-        
+        ''' LSTM Output
+        # output = (seq_len, batch, num_directions_hidden_size) from the last layer of the lstm
+        # h_n = (num_layers * num_directions, batch, hidden_size) a tensor containing the hidden state for t = seq_len
+        #   hidden states are the
+        # c_n = (num_layers * num_directions, batch, hidden_size) a tensor containing the cell state for t = seq_len 
+        #   cell states maintain the bulk of the networks information as it transfers relevant information throughout the process, which get altered by the various gates.
+            This is how information from earlier 
+        '''
         # Calculations performed in each layer
         if self.bidirectional:
             self.fc = nn.Linear(hidden_dim*2, output_size)
         else: self.fc = nn.Linear(hidden_dim, output_size)
+        ''' Linear class
+            
+        '''
+        if weight !=0:
+            self.init_weights()
 
     def forward(self, x):
         
@@ -74,36 +86,23 @@ class LSTM(nn.Module):
             c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_().to(device)
         # Generate output, hidden and cell states
         out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))       # detach is required to ensure BPTT is done only in the current batch and not all batches.
-        ''' LSTM Output
-        # output = (seq_len, batch, num_directions_hidden_size) from the last layer of the lstm
-        # h_n = (num_layers * num_directions, batch, hidden_size) a tensor containing the hidden state for t = seq_len
-        # c_n = (num_layers * num_directions, batch, hidden_size) a tensor containing the cell state for t = seq_len 
-        '''
+        
         return self.fc(out[:,-1,:])                                    # only inputs the final layer of elements.
-
+    '''
+        weights are automatically assigned through nn.Linear() ---> self.weight = Parameter(torch.Tensor(out_features, in_features))
+        Parameter is a class that assigns model attributes which adds them to the list of model parametes.
+        A seperate function must be made to select a specific weight initialisation
+    '''
     # Initialise weights
     def init_weights():
+        int_range = 1
+        self.weight.data.uniform_(-int_range, int_range)
+        '''
+            .data changes the values of the tensor
+            .uniform_(from, to) fills the tensor with numbers from a continuous uniform distribution (1/(to - from))
+        '''
         return 0
 
-    # Initialise hidden states
-    def init_hidden(self, batch_size):
-        ''' Initializes hidden state '''
-        # Create two new tensors with sizes n_layers x batch_size x hidden_dim,
-        # initialized to zero, for hidden state and cell state of LSTM
-        weight = next(self.parameters()).data
-        
-        layer_mult = 1
-        if self.bidirectional:
-            layer_mult *= 2
-        
-        if (torch.cuda.is_available()):
-            hidden = (weight.new(self.n_layers * layer_mult, batch_size, self.hidden_dim).zero_().cuda(),
-                  weight.new(self.n_layers * layer_mult, batch_size, self.hidden_dim).zero_().cuda())
-        else:
-            hidden = (weight.new(self.n_layers * layer_mult, batch_size, self.hidden_dim).zero_(),
-                      weight.new(self.n_layers * layer_mult, batch_size, self.hidden_dim).zero_())
-        
-        return hidden
 # Step 4: Instantiate Model Class
 
 # hyper-parameters
@@ -116,7 +115,7 @@ learning_rate = 0.1
 
 # instantiate model
 model = LSTM(input_size, hidden_dim, layer_dim, output_size, True).to(device)
-
+x = LSTM()
 # Step 5: Instantiate Loss Class
 criterion = nn.CrossEntropyLoss()
 
